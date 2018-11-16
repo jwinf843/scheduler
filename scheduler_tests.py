d7b07d94cd3550ae00f89ce5b4ba062afaab3325
Raghu Reddy <rreddy@gobalto.com>
2018-11-15 16:16:35 -0800

    Update tests

--- scheduler_tests.py
+++ scheduler_tests.py
@@ -1,9 +1,37 @@
+import pytest
 from scheduler import ScheduleParser
 
-physics_classes = ScheduleParser('physics.json')
-physics_classes.read_contents()
-physics_classes.reorder() == ['Calculus', 'Scientific Thinking','Differential Equations','Intro to Physics','Relativity']
+def setup_math_parser():
+    math = ScheduleParser('math.json')
+    math.read_contents()
+    return math
 
-math_classes = ScheduleParser('math.json')
-math_classes.read_contents()
-math_classes.reorder() == ['Algebra 1', 'Geometry', 'Algebra 2', 'Pre Calculus']
+def setup_physics_parser():
+    physics = ScheduleParser('physics.json')
+    physics.read_contents()
+    return physics
+
+
+def test_read_contents():
+    physics_classes = setup_physics_parser()
+    assert(physics_classes.class_list == [{'name': 'Calculus', 'prerequisites': []},
+        {'name': 'Scientific Thinking', 'prerequisites': []},
+        {'name': 'Differential Equations', 'prerequisites': ['Calculus']},
+        {'name': 'Intro to Physics', 'prerequisites': ['Scientific Thinking']},
+        {'name': 'Relativity', 'prerequisites': ['Differential Equations', 'Intro to Physics']}
+    ]) is True
+
+    math_classes = setup_math_parser()
+    assert(math_classes.class_list == [
+        {'name': 'Algebra 1', 'prerequisites': []},
+        {'name': 'Geometry', 'prerequisites': []},
+        {'name': 'Algebra 2', 'prerequisites': ['Algebra 1', 'Geometry']},
+        {'name': 'Pre Calculus', 'prerequisites': ['Algebra 2']}
+    ]) is True
+
+def test_reorder_of_classes():
+    physics_classes = setup_physics_parser()
+    assert(physics_classes.reorder() == ['Calculus', 'Scientific Thinking','Differential Equations','Intro to Physics','Relativity']) is True
+
+    math_classes = setup_math_parser()
+    assert(math_classes.reorder() == ['Algebra 1', 'Geometry', 'Algebra 2', 'Pre Calculus']) is True
